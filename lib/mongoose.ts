6ca86fdc9cd9bc3b0e01cf7b913502983f26804e
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-19 23:33:32 +0700

    update

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -5,4 +5,8 @@ let isConnected = false; // variable to check if mongoose is connected
 // create connection DB function
 export const connectedToDB = async () => {
     mongoose.set('strictQuery', true); // this is to prevent unknown field queries
+
+    // Apakah url mongodb secara spesifik terhubung
+    if (!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
+    if (isConnected) return console.log('Already connected to MongoDB');
 }
