3e272753a01ff9b6e15bdd81262a0efd988a9c0f
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-19 20:51:25 +0700

    update

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -17,7 +17,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { UserValidation } from "@/lib/validations/user";
 import * as z from "zod";
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 
 interface Props {
     user: {
@@ -33,20 +33,40 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+    const [files, setFiles] = useState<File[]>([]);
+
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
             profile_photo: user?.image || "",
             name: user?.name || "",
             username: user?.username || "",
-            bio: user?.bio || "",
+            bio: user?.bio || ""
         }
     });
 
     // void -> tidak mengembalikan apapun
     // cara react hook form bekerja untuk mendapatkan segera value-nya di sini
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault(); // mencegah browser me-reload sendiri ketika meng-upload image
+
+        const fileReader = new FileReader();
+
+        if (e.target.files && e.target.files.length > 1) {
+            const file = e.target.files[0]
+
+            setFiles(Array.from(e.target.files));
+
+            if (!file.type.includes('image')) return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || '';
+
+                fieldChange(imageDataUrl);
+            }
+
+            fileReader.readAsDataURL(file);
+        }
     }
 
     function onSubmit(values: z.infer<typeof UserValidation>) {
