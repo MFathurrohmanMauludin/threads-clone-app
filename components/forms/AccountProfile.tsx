63a018bff985f15b21bab7e28c0fc0a2cbfa7d3f
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-20 11:22:40 +0700

    update

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
     user: {
@@ -37,6 +39,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const [files, setFiles] = useState<File[]>([]);
     const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -87,6 +91,14 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         }
 
         // TODO: Update user profile
+        await updateUser(
+            values.username,
+            values.name,
+            values.bio,
+            values.profile_photo,
+            user.id,
+            pathname,
+        );
     }
 
     return (
