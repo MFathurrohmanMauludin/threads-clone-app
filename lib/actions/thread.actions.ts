46f2889bf212083d9658bc0891e6f1e7dd88dd9b
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-23 20:21:53 +0700

    update

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,4 +1,7 @@
+import { revalidatePath } from "next/cache";
+import Thread from "../models/thread.model";
 import { connectToDB } from "../mongoose";
+import User from "../models/user.model";
 
 interface Params {
     text: string,
@@ -11,5 +14,17 @@ export async function createThread({ text, author, communityId, path }: Params) 
     connectToDB();
 
     // Thread ini adalah mongoose mongodb model
-    const createThread = await Thread.create();
+    const createThread = await Thread.create({
+        text,
+        author,
+        community: null,
+    });
+
+    // Update user model
+    await User.findByIdAndUpdate(author, {
+        $push: { threads: createThread._id }
+    })
+
+    // allows you to purge cached data on-demand for a specific path
+    revalidatePath(path);
 }
