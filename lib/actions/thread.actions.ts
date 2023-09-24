df362d17d3cf99aab3d61ba641827669d54ea7a0
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-24 10:46:58 +0700

    update

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -48,4 +48,22 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
     const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
         .sort({ createdAt: 'desc' })
         .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User })
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: "_id name parentId image"
+            }
+        })
+
+    const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext }
 }
