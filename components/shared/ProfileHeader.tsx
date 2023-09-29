41d8671d61d700e603bf3a3d8817e6cd7d7c9d2e
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-09-29 18:54:56 +0700

    update

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface Props {
     accountId: string;
     authUserId: string;
@@ -18,7 +20,16 @@ const ProfileHeader = ({
     return (
         <div className="flex w-full flex-col justify-start">
             <div className="flex items-center justify-between">
-
+                <div className="flex items-center gap-3">
+                    <div className="relative h-20 w-20 object-cover">
+                        <Image
+                            src={imgUrl}
+                            alt="Profile Image"
+                            fill
+                            className="rounded-full object-cover shadow-2xl"
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     )
