603e37db9b4bef61c653e642cf9c4e93ce94cbb2
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-10-01 05:45:45 +0700

    update

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,3 +1,6 @@
+import { fecthUserProfile } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+
 interface Props {
     currentUserId: string;
     accountId: string;
@@ -6,6 +9,9 @@ interface Props {
 
 const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
     // TODO: Fecth profile threads
+    let result = await fecthUserProfile(accountId);
+
+    if (!result) redirect('/');
 
     return (
         <section>
