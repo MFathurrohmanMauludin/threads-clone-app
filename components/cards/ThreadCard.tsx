30f3ba49a94f8cace89d2e71a058e7d5ec0435ca
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-10-04 15:16:00 +0700

    update

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -99,24 +99,23 @@ const ThreadCard = ({
 
                 {/* TODO: Delete Thread  */}
                 {/* TODO: show comment logo */}
-
-                {!isComment && community && (
-                    <Link href={`/comunities/${community.id}`} className="mt-5 flex items-center">
-                        <p className="text-subtle-medium text-gray-1">
-                            {formatDateString(createdAt)}
-                            - {community.name} Community
-                        </p>
-
-                        <Image
-                            src={community.image}
-                            alt={community.name}
-                            width={14}
-                            height={14}
-                            className="ml-1 rounded-full object-cover"
-                        />
-                    </Link>
-                )}
             </div>
+            {!isComment && community && (
+                <Link href={`/comunities/${community.id}`} className="mt-5 flex items-center">
+                    <p className="text-subtle-medium text-gray-1">
+                        {formatDateString(createdAt)}
+                        {" "} | {community.name} Community
+                    </p>
+
+                    <Image
+                        src={community.image}
+                        alt={community.name}
+                        width={14}
+                        height={14}
+                        className="ml-1 rounded-full object-cover"
+                    />
+                </Link>
+            )}
         </article>
     )
 }
